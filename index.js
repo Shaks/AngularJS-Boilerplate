e58d64dcff4a4a357f71e1d60f1dca2f885125f1
Shakir Windows <shakirkhan02@yahoo.com>
2014-10-02 23:48:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express"),
+    fs = require('fs'),
+    port = process.env.PORT || 2595;
+
+//var logger = require('morgan');
+var bodyParser = require('body-parser')
+
+var app = express();
+app.use(bodyParser.json());
+app.set("view options", {
+    layout: false
+});
+app.use(express.static(__dirname + '/public'));
+ 
+app.get('/', function (req, res) {
+    res.render('public/index.html');
+});
+ 
+app.listen(port);
+console.log('Express server running at http://localhost:' + port);
